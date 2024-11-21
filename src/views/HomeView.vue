106f6934746174a710caf145985e3eaa59b45a99
arielglvz <arielglvz@gmail.com>
2024-11-21 12:52:54 +0800

    Setting up the app and adding Composition AP

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  // Composition API:
+  setup() {
+    let name = 'Mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('You clicked me')
+    }
+    return { name, age, handleClick}
+  },
+}
+</script>
