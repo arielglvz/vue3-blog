fd9db7d4413210ecf8d2309d5f9bfc62cb9d5900
arielglvz <arielglvz@gmail.com>
2024-11-21 14:27:31 +0800

    Added template refs for reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="home">
     home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click Me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name"/>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 // @ is an alias to /src
 
 export default {
-  name: 'HomeView',
+
+  name: 'Home',
   // Composition API:
   setup() {
-    let name = 'Mario'
-    let age = 30
+    
+    // Using Refs for reactivity
+    const name = ref('Mario')
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log('You clicked me')
+      name.value = 'luigi'
+      age.value = 35
     }
-    return { name, age, handleClick}
+
+    return { name, age, handleClick }
   },
 }
 </script>
