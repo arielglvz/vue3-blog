601057f48840a94c4f705adf14a4672734af8a40
arielglvz <arielglvz@gmail.com>
2024-11-21 21:54:59 +0800

    Computed Values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
+import { computed, ref } from 'vue';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad',  'bowser', 'koopa', 'peach',])
+
+    // Computed Values
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return { names, search, matchingNames }
+  }
 }
 </script>
 
