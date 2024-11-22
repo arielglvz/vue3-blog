77399873c082d055a62967bf7c05effad6dd880e
arielglvz <arielglvz@gmail.com>
2024-11-22 14:42:05 +0800

    Reusable Composable

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="post">
-    <h3>{{ post.title }}</h3>
+    <router-link :to="{ name: 'Details', params: { id: post.id }}">
+      <h3>{{ post.title }}</h3>
+    </router-link>
     <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">
+      #{{ tag }}
+    </span>
   </div>
 </template>
 
@@ -15,7 +20,7 @@ export default {
       if(props.post.body.length > 100) {
         return props.post.body.substring(0, 100) + '....'
       }
-      
+
       return props.post.body
     })
 
